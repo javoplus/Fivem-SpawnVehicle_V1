0348531b5a95ca6d7018a940547454058778da48
Javo Plus <80983935+javoplus@users.noreply.github.com>
2021-03-21 03:13:28 +0200

    Update and rename __resource.lua to fxmanifest.lua

--- __resource.lua
+++
@@ -1,4 +0,0 @@
-version '1.0.0'
-author 'Javo Plus'
-
-client_script 'cl-spawnvehicle.lua'
---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+name "Spawn Vehicle"
+author "Javo Plus"
+version "1.0"
+
+client_script 'cl-spawnvehicle.lua'
