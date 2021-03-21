7149bd493df8eaeb843951556b9fe2079e28fa1b
Javo Plus <80983935+javoplus@users.noreply.github.com>
2021-03-21 03:54:57 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,7 @@
 fx_version 'cerulean'
-
 game { 'gta5' }
-
-lua54 'yes'
-
 author "Javo Plus"
-
 version '1.0'
-
-description 'Spawn Vehicle'
+name 'Spawn Vehicle'
 
 client_script 'cl-spawnvehicle.lua'
